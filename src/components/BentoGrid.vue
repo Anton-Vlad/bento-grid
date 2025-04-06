d66abd96246fe97382c5336de4513fec72b87db2
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2025-04-06 14:48:47 +0300

    bento grid done

---
+++ src/components/BentoGrid.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="bento-grid">
+        <GridBox>
+            <h1>Social Media <span>10x</span> Faster with AI</h1>
+            <img src="/src/assets/images/illustration-five-stars.webp" alt="" />
+            <p>Over 4,000 5-star reviews</p>
+        </GridBox>
+        
+        <GridBox>
+            <h2>Manage multiple accounts and platforms.</h2>
+            <img src="/src/assets/images/illustration-multiple-platforms.webp" alt="" />
+        </GridBox>
+
+        <GridBox>
+            <h2>Maintain a consistent posting schedule.</h2>
+            <img src="/src/assets/images/illustration-consistent-schedule.webp" alt="" />
+        </GridBox>
+        
+    
+        <GridBox>
+            <h2>Schedule to social media.</h2>
+            <img src="/src/assets/images/illustration-schedule-posts.webp" alt="" />
+            <p>Optimize post timings to publish content at the perfect time for your audience. </p>
+        </GridBox>
+
+        <GridBox>
+            <h2>Grow followers with non-stop content.</h2>
+            <img src="/src/assets/images/illustration-grow-followers.webp" alt="" />
+        </GridBox>
+    
+
+        <GridBox>
+            <h2>>56% <span>faster audience growth</span></h2>
+            <img src="/src/assets/images/illustration-audience-growth.webp" alt="" />
+        </GridBox>
+
+        <GridBox>
+            <h2>Create and schedule content <span>quicker.</span></h2>
+            <img src="/src/assets/images/illustration-create-post.webp" alt="" />
+        </GridBox>
+        
+        <GridBox>
+            <h2>Write your content using AI.</h2>
+            <img src="/src/assets/images/illustration-ai-content.webp" alt="" />
+        </GridBox>
+    </div>
+</template>
+
+<script setup>
+import GridBox from './GridBox.vue';
+</script>
